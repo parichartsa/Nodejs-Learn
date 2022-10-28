e5ad3f505404daf4fabd9c6ae4a850892c7525ff
onicha1010 <teenoum1010@gmail.com>
2022-10-28 14:27:28 +0700

    53%_lec16
    template engine
    setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path')
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
+
+app.set("views","./src/views");
+app.set("view engine", "ejs");
+
+app.get("/", (req,res) => {
+
+    res.render('index', {username : "Onicha", friend : ["Tah", "Alitah", "On"]})
+})
+
+app.listen(PORT, () => {
+
+    debug("listening on PORT" + chalk.green(" : " + PORT));
+})
